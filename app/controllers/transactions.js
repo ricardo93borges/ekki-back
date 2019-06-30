5015e3474429d4a4d941a96f9c75b3d777c70426
ricardo93borges <ricardo93borges@gmail.com>
2019-06-30 01:06:10 -0300

    Implemented transactions business logic

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -1,19 +1,21 @@
 const { Op } = require('sequelize');
 const { Transaction } = require('../models')
+const { Account } = require('../models')
+const { Status } = require('../models')
 
 exports.get = async (req, res) => {
     try {
-
         const page = req.params.page
         const limit = 10
         const offset = limit * (page - 1)
+        const accountId = req.params.account_id
 
         const total = await Transaction.count({
-            where: { [Op.or]: [{ from_account_id: req.params.account_id }, { to_account_id: req.params.account_id }] }
+            where: { [Op.or]: [{ from_account_id: accountId }, { to_account_id: accountId }] }
         })
 
         const transactions = await Transaction.findAll({
-            where: { [Op.or]: [{ from_account_id: req.params.account_id }, { to_account_id: req.params.account_id }] },
+            where: { [Op.or]: [{ from_account_id: accountId }, { to_account_id: accountId }] },
             offset, limit
         })
 
@@ -23,10 +25,36 @@ exports.get = async (req, res) => {
         res.status(500).send(err.errors);
     }
 }
-
+/**
+ * TODO se existe uma transaction com o mesmo valor feita a menos de 2 minutos atras:
+ *  cancelar transaction antiga
+ *  criar uma nova no lugar
+ */
 exports.create = async (req, res) => {
     try {
-        const transaction = await Transaction.create(req.body)
+        const fromAccount = await Account.findByPk(req.body.fromAccountId)
+        const toAccount = await Account.findByPk(req.body.toAccountId)
+        const status = await Status.findOne({ where: { name: 'success' } })
+        const amount = req.body.amount
+
+        //If account has sufficient balance
+        if (Number(fromAccount.balance) >= amount) {
+            const transaction = await Transaction.add(fromAccount, toAccount, status, amount)
+            res.status(201).send(transaction)
+        }
+
+        const cash = (Number(fromAccount.limit) + Number(fromAccount.balance))
+
+        //If account has sufficient funds
+        if (cash > amount) {
+            const limit = fromAccount.limit - (amount - fromAccount.balance)
+            const transaction = await Transaction.add(fromAccount, toAccount, status, amount, limit)
+            res.status(201).send(transaction)
+        } else {
+            res.status(500).send({ errors: [{ message: 'insufficient funds' }] })
+        }
+
+        const transaction = await Transaction.add(req.body.amount, req.body.fromAccountId, req.body.toAccountId)
         res.status(201).send(transaction)
     } catch (err) {
         console.log(err)
--- app/models/transactions.js
+++ app/models/transactions.js
@@ -12,15 +12,18 @@ module.exports = (sequelize, DataTypes) => {
         },
         statusId: {
             type: DataTypes.INTEGER,
-            field: 'status_id'
+            field: 'status_id',
+            allowNull: false
         },
         fromAccountId: {
             type: DataTypes.INTEGER,
-            field: 'from_account_id'
+            field: 'from_account_id',
+            allowNull: false
         },
         toAccountId: {
             type: DataTypes.INTEGER,
-            field: 'to_account_id'
+            field: 'to_account_id',
+            allowNull: false
         },
         createdAt: {
             type: DataTypes.DATE,
@@ -37,6 +40,26 @@ module.exports = (sequelize, DataTypes) => {
         Transaction.belongsTo(models.Status, { as: 'status' })
         Transaction.belongsTo(models.Account, { as: 'from_account' })
         Transaction.belongsTo(models.Account, { as: 'to_account' })
+    }
+
+    Transaction.add = async (fromAccount, toAccount, status, amount, limit = null) => {
+        try {
+
+            if (limit) {
+                await fromAccount.update({ balance: 0, limit })
+            } else {
+                await fromAccount.update({ balance: (Number(fromAccount.balance) - amount) })
+            }
+
+            await toAccount.update({ balance: (Number(toAccount.balance) + amount) })
+
+            const transaction = await Transaction.create({ fromAccountId: fromAccount.id, toAccountId: toAccount.id, amount, statusId: status.id })
+            return transaction
+
+        } catch (err) {
+            console.log(err)
+            return err
+        }
     }
 
     return Transaction
