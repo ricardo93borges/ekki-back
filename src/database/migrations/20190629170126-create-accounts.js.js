28fc4c38d3b00b1decb5290333c0da36d9211cec
ricardo93borges <ricardo93borges@gmail.com>
2019-06-30 21:48:13 -0300

    fixing seeders

--- src/database/migrations/20190629170126-create-accounts.js.js
+++ src/database/migrations/20190629170126-create-accounts.js.js
@@ -25,12 +25,14 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        field: 'created_at'
+        field: 'created_at',
+        defaultValue: Sequelize.literal('NOW()')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        field: 'updated_at'
+        field: 'updated_at',
+        defaultValue: Sequelize.literal('NOW()')
       },
     })
   },
