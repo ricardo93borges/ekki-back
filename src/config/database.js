eb162ac0304def247a71a2621cb2522f53779acc
ricardo93borges <ricardo93borges@gmail.com>
2019-06-30 21:18:09 -0300

    deploy setup

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ module.exports = {
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT || 'mysql',
+  dialect: process.env.DB_DIALECT || 'postgres',
   storage: './__tests__/database.sqlite',
   define: {
     timestamps: true,
