2639e545be08f6cf3043a52ac88592c401e98faa
ricardo93borges <ricardo93borges@gmail.com>
2019-07-02 13:45:19 -0300

    using cors

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ require("dotenv").config({
 })
 
 const express = require('express')
+const cors = require('cors')
+
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 require('./app/routes/index')(app)
 
