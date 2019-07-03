83d2be1e52ad7fe7bcdbc41bc83afc44807505ab
ricardo93borges <ricardo93borges@gmail.com>
2019-07-03 20:55:31 -0300

    fixing cors problem

--- src/app/routes/users.js
+++ src/app/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/users')
+const cors = require('cors')
 
-router.get('/', controller.all)
+router.get('/', cors(), controller.all)
 router.get('/:id', controller.get)
 router.post('/', controller.create)
 
